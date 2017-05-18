b7eaaa54840fcbd076643f9447bf703a352ea56f
Dicky Chiang <dicky_chiang@ilitek.com>
2017-05-18 18:05:07 +0800

    combined iram upgrade to func but not test yet.

--- core/firmware.h
+++ core/firmware.h
@@ -25,6 +25,12 @@ typedef struct _CORE_FIRMWARE {
 
 	uint32_t df_crc;
 
+	uint32_t start_addr;
+
+	uint32_t end_addr;
+
+	uint32_t checksum;
+
 	//int size;
 
 	uint8_t *fw_data_max_buff;
@@ -35,11 +41,11 @@ typedef struct _CORE_FIRMWARE {
 
 	bool isCRC;
 
-	int (*upgrade_func)(uint8_t *FwData);
+	int (*upgrade_func)(uint8_t *FwData, uint8_t *iram_data);
 
 } CORE_FIRMWARE;
 
-extern int core_firmware_iram_upgrade(const char* fpath);
+//extern int core_firmware_iram_upgrade(const char* fpath);
 extern int core_firmware_upgrade(const char* fpath);
 extern int core_firmware_init(uint32_t id);
 extern void core_firmware_remove(void);
--- platform.c
+++ platform.c
@@ -7,7 +7,7 @@
 
 #include "platform.h"
 
-// Modify it if want to change the supprot of an IC by the driver
+// Modify it if want to change the support of an IC by the driver
 #define ON_BOARD_IC		CHIP_TYPE_ILI7807
 //#define ON_BOARD_IC		CHIP_TYPE_ILI2121
 
